49c793dfc734c80def17d388ebd9a8421fc771a7
Ryo Kuroyanagi <im.ryokuro@gmail.com>
2023-09-04 23:55:20 +0900

    added file size limit protection

--- server.py
+++ server.py
@@ -13,5 +13,8 @@ def read_root():
 async def removeBg(request: Request, file: UploadFile = File(...)):
     if request.headers["X-API-KEY"] != "secret":
         raise HTTPException(status_code=401, detail="Unauthorized")
-    result = remove(file.file.read())
+    b = file.file.read()
+    if len(b) > 1024 * 1024 * 1:
+        raise HTTPException(status_code=400, detail="File size should be less than 5MB")
+    result = remove(b)
     return StreamingResponse(content=BytesIO(result), media_type="application/octet-stream")
